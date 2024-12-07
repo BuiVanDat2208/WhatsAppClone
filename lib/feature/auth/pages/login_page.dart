fd2f1405d9acc5f83aeaf990e4194e97f0430180
BuiVanDat2208 <buivandat2003hn@gmail.comm>
2024-12-07 10:59:10 +0700

    Build a login and connect using the phone number.

---
+++ lib/feature/auth/pages/login_page.dart
@@ -0,0 +1,196 @@
+import 'package:country_picker/country_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/common/extension/custom_theme_extension.dart';
+import 'package:whatsapp_clone/common/helper/show_alert_dialog.dart';
+import 'package:whatsapp_clone/common/utils/coloors.dart';
+import 'package:whatsapp_clone/common/widget/custom_elevated_button.dart';
+import 'package:whatsapp_clone/common/widget/custom_icon_button.dart';
+import 'package:whatsapp_clone/feature/auth/widget/custom_text_field.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  late TextEditingController countryNameController;
+  late TextEditingController countryCodeController;
+  late TextEditingController phoneNumberController;
+
+  // Click the check button to check if the phone number is compatible, then let it go.
+  sendCodeToPhone() {
+    // ignore: unused_local_variable
+    final phone = phoneNumberController.text;
+    final name = countryNameController.text;
+
+    if (phone.isEmpty) {
+      return showAlertDialog(
+          context: context, messages: "Please enter your phone number");
+    } else if (phone.length < 9) {
+      return showAlertDialog(
+          context: context,
+          messages:
+              "The phone number you entered is too short for the country: $name \n\n");
+    } else if (phone.length > 10) {
+      return showAlertDialog(
+          context: context,
+          messages:
+              "The phone number you entered us too long for the country: $name");
+    }
+  }
+
+  //
+  showCountryCodePicker() {
+    showCountryPicker(
+      context: context,
+      showPhoneCode: true,
+      favorite: ['VN'],
+      countryListTheme: CountryListThemeData(
+        bottomSheetHeight: 600,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        flagSize: 22,
+        borderRadius: BorderRadius.circular(20),
+        inputDecoration: InputDecoration(
+          labelStyle: TextStyle(color: context.theme.greyColor),
+          prefixIcon: const Icon(
+            Icons.language,
+            color: Coloors.greenDark,
+          ),
+          hintText: "Search county code or name",
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: context.theme.greyColor!.withOpacity(0.2),
+            ),
+          ),
+          focusedBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Coloors.greenDark,
+            ),
+          ),
+        ),
+      ),
+      onSelect: (country) {
+        countryNameController.text = country.name;
+        countryCodeController.text = country.countryCode;
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    countryNameController = TextEditingController(text: "VietNam");
+    countryCodeController = TextEditingController(text: "84");
+    phoneNumberController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    countryNameController.dispose();
+    countryCodeController.dispose();
+    phoneNumberController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
+        title: Text(
+          "Enter you's phone number",
+          style: TextStyle(
+            color: context.theme.authAppbarTextColor,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          CustomIconButton(
+            onTap: () {},
+            icon: Icons.more_vert,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                text: "WhatsApp will need to verify your phone number.",
+                style: TextStyle(
+                  color: context.theme.greyColor,
+                  height: 1.5,
+                ),
+                children: [
+                  TextSpan(
+                    text: "What's my number?",
+                    style: TextStyle(
+                      color: context.theme.blueColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: CustomTextField(
+              onTap: showCountryCodePicker,
+              controller: countryNameController,
+              readOnly: true,
+              suffixIcon: const Icon(
+                Icons.arrow_drop_down,
+                color: Coloors.greenDark,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 70,
+                  child: CustomTextField(
+                    onTap: showCountryCodePicker,
+                    controller: countryCodeController,
+                    prefixText: '+',
+                    readOnly: true,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: CustomTextField(
+                    controller: phoneNumberController,
+                    hintText: "PhoneNumber",
+                    textAlign: TextAlign.left,
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            "Carrier charges may apply",
+            style: TextStyle(
+              color: context.theme.greyColor,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: CustomElevatedButton(
+        onPressed: sendCodeToPhone,
+        text: "Next",
+        buttonWidth: 90,
+      ),
+    );
+  }
+}
