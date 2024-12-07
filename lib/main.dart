a52b48520254b0c587e12011809e3fbb8295fb38
BuiVanDat2208 <buivandat2003hn@gmail.comm>
2024-12-07 10:42:24 +0700

    build verification page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/common/theme/dark_theme.dart';
 import 'package:whatsapp_clone/common/theme/light_theme.dart';
+import 'package:whatsapp_clone/feature/auth/pages/login_page.dart';
+import 'package:whatsapp_clone/feature/auth/pages/verification_page.dart';
 import 'package:whatsapp_clone/feature/welcome/pages/welcome_page.dart';
 
 void main() {
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: lightTheme(),
       darkTheme: darkTheme(),
       themeMode: ThemeMode.system,
-      home: const WelcomePage(),
+      home: const VerificationPage(),
     );
   }
 }
